da49b63751f4c73d71330761f3f9da486d3cdd17
norush <a123>
2024-03-06 11:40:15 +0800

    fix: introduce @antfu/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,10 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
+  extends: [
+      "@repo/eslint-config/library.js",
+      "@repo/eslint-config/antfu-eslint.js",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
